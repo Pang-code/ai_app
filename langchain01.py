10872212e9761b443a4869bbbf24af8fbcdfd098
pcw <m13421653181@163.com>
2026-06-08 14:23:41 +0800

    feat:

---
+++ langchain01.py
@@ -0,0 +1,22 @@
+# pip install -qU langchain "langchain[openai]"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import create_agent
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    # model="deepseek:deepseek-chat",
+    model="deepseek-chat",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
+)
+print(result["messages"][-1].content_blocks)
