b2d5a17e5ce42585e5da9d672b847e668fd873ca
pangcw <pangcw001@outlook.com>
2026-06-13 22:24:14 +0800

    refactor(agent): 更新智能体工具配置并清理代码
    - 移除旧的网络搜索工具实现代码
    - 添加新的 MySearchTool 工具类引用
    - 修改智能体创建逻辑以使用新工具
    - 清理过时的导入和注释代码

--- langgraph_code/src/agent/my_agent2.py
+++ langgraph_code/src/agent/my_agent2.py
@@ -1,11 +1,15 @@
 from langchain.agents import create_agent
 from agent.init_llm import deepseek_llm
 from agent.tools.tool_demo2 import web_search
+from agent.tools.tool_demo3 import MySearchTool
+
+my_search=MySearchTool()  #
 
 # 创建智能体实例
 agent = create_agent(
     # "qwen3.7-max",
     deepseek_llm,
-    tools=[web_search],
+    # tools=[web_search, MySearchTool],
+    tools=[ MySearchTool],
     system_prompt="你是一个智能助手。尽可能的调用工具回答用户的问题"
 )
--- langgraph_code/src/agent/tools/tool_demo2.py
+++ langgraph_code/src/agent/tools/tool_demo2.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 
 from langchain_core.tools import tool
-from zhipuai import api_key
 
 from agent.init_llm import zhipu_llm as zhipuai_client
 
@@ -53,41 +52,6 @@ if __name__ == '__main__':
 
 
 
-
-
-
-#  类的形式
-# from pydantic import BaseModel, Field
-# from langchain.tools import BaseTool, Type
-# from zhipuai import ZhipuAI
-#
-# # 网络搜索工具的参数模型
-# class SearchArgs(BaseModel):
-#     query: str = Field(description="需要进行网络搜索的信息。")
-#
-# # 网络搜索的工具
-# class MySearchTool(BaseTool):
-#     # 工具名字
-#     name: str = "search_tool"
-#     description: str = '搜索互联网上公开内容的工具'
-#     return_direct: bool = False
-#     args_schema: Type[BaseModel] = SearchArgs
-#
-#     def _run(self, query) -> str:
-#         try:
-#             # print("执行我的Python中的工具, 输入的参数为:", query)
-#             response = zhipuai_client.web_search.web_search(
-#                 search_engine="search_pro",
-#                 search_query=query
-#             )
-#             # print(response)
-#             if response.search_result:
-#                 return "\n\n".join([d.content for d in response.search_result])
-#             return '没有搜索到任何内容！'
-#         except Exception as e:
-#             print(e)
-#             return '没有搜索到任何内容！'
-#
 
 
 
