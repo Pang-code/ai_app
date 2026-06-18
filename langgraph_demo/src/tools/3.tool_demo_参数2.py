5a466fcfe19aef14629659cd2ce44528a96212b0
pangcw <pangcw001@outlook.com>
2026-06-18 22:55:22 +0800

    feat(tools): 添加计算工具函数
    - 实现了支持加减乘除四则运算的工具函数
    - 集成 LangChain Core 的 @tool 装饰器功能
    - 添加了详细的参数注解和类型提示
    - 实现了运算类型匹配和异常处理逻辑
    - 集成了 JSON Schema 验证和直接返回功能
    - 添加了工具名称、描述和参数的调试输出功能

---
+++ langgraph_demo/src/tools/3.tool_demo_参数2.py
@@ -0,0 +1,36 @@
+from langchain_core.tools import tool
+from typing import Annotated
+
+@tool(name="calculate3", parse_docstring=True)
+def calculate3(a: Annotated[float, '第一个需要输入的数字。'],
+              b: Annotated[float, '第二个需要输入的数字。'],
+              operation: Annotated[str, '运算类型，只能是add、subtract、multiply和divide中的任意一个。']
+              ) -> float:
+    """工具函数：计算两个数字的运算结果"""
+
+    print(f"调用 calculate 工具，第一个数字：{a}，第二个数字：{b}，运算类型：{operation}")
+
+    result = 0.0
+    match operation:
+        case "add":
+            result = a + b
+        case "subtract":
+            result = a - b
+        case "multiply":
+            result = a * b
+        case "divide":
+            if b != 0:
+                result = a / b
+            else:
+                raise ValueError("除数不能为零")
+    return result
+
+
+print(calculate3.name)
+# 输出: calculate
+print(calculate3.description)
+# 输出: 工具函数：计算两个数字的运算结果
+print(calculate3.args)
+print(calculate3.args_schema.model_json_schema())
+print(calculate3.return_direct)
+print(calculate3.invoke({'a': 40, 'b': 2, 'operation': 'multiply'}))
