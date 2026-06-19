6c602f364be13089543418a1636c96282212c7f9
pangcw <pangcw001@outlook.com>
2026-06-20 01:07:37 +0800

    feat(agent): 添加MCP代理支持多服务器配置
    - 集成MultiServerMCPClient支持Python、Java和智谱AI多种MCP服务端
    - 配置SSE和streamable_http两种传输协议
    - 实现异步获取工具、提示和资源的功能
    - 创建基于React的智能代理实例
    - 集成qwen_llm作为默认语言模型

---
+++ langgraph_demo/src/agent/mcp_agent8.py
@@ -0,0 +1,73 @@
+import asyncio
+
+from langgraph.prebuilt import create_react_agent
+
+from agent.init_llm import qwen_llm as llm
+
+# Python MCP 服务端的连接配置  sse
+# python_mcp_server_config = {
+#     'url': 'http://127.0.0.1:8080/sse',
+#     'transport': 'sse',
+# }
+
+# # Python MCP 服务端的连接配置  streamable
+python_mcp_server_config = {
+    'url': 'http://127.0.0.1:8080/streamable',
+    'transport': 'streamable_http',
+}
+
+java_mcp_server_config = {
+    'url': 'http://127.0.0.1:8080/sse',
+    'transport': 'sse',
+}
+
+# 外网上公开 MCP 服务端的连接配置
+zhipuai_mcp_server_config = {
+    'url': "https://open.bigmodel.cn/api/mcp/web/web_search/sse?Authorization=YOUR API Key",
+    'transport': 'sse',
+}
+
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+# MCP的客户端
+mcp_client = MultiServerMCPClient(
+    {
+        'python_mcp': python_mcp_server_config,
+        'java_mcp': java_mcp_server_config,
+        'zhipuai_mcp': zhipuai_mcp_server_config,
+    }
+)
+
+
+async def create_agent():
+    """必须是异步函数中"""
+    mcp_tools = await mcp_client.get_tools()
+
+    print(mcp_tools)
+
+    p = await mcp_client.get_prompt(
+        server_name='python_mcp',  # mcp 服务端名称
+        prompt_name='ask_about_topic',
+        arguments={'topic': '深度学习'}
+    )
+
+    print(p)
+    data = await mcp_client.get_resources(
+        server_name='python_mcp',  # mcp 服务端名称
+        uris='resource:///config'
+    )
+    print(data[0])
+    print(data[0].data)
+
+    my_agent7 = create_react_agent(
+        llm,
+        tools=mcp_tools,
+        prompt="你是一个智能助手,尽可能的调用工具回答用户的问题",
+
+    )
+
+    return my_agent7
+
+
+agent = asyncio.run(create_agent())
